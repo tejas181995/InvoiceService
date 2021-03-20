5dedfea9248517e6c7d3eeaa12ce7bc5da985906
Tejas <tejasdev95@gmail.com>
2021-03-20 17:48:40 +0530

    Added method to calculate fare for premium customr

--- src/main/java/com/bridgelabz/invoiceservice/InvoiceService.java
+++ src/main/java/com/bridgelabz/invoiceservice/InvoiceService.java
@@ -1,17 +1,20 @@
 package com.bridgelabz.invoiceservice;
 
 public class InvoiceService {
-    private static final int MINIMUM_COST_PER_KM = 10;
-    private static final int COST_PER_TIME = 1;
-    private static final double MINIMUM_FARE = 5.0;
+    private static final int MINIMUM_COST_PER_KM [] = new int[]{10,15};
+    private static final int COST_PER_TIME [] = new int[]{1, 2};
+    private static final double MINIMUM_FARE []= new double[]{5.0, 20.0};
 
-    public double calculateFare(double distance, int time) {
-        return Math.max((distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME), MINIMUM_FARE);
+    public static final int NORMAL_RIDE = 0, PREMIUM_RIDE = 1;
+
+
+    public double calculateFare(double distance, int time, int rideType) {
+        return Math.max((distance * MINIMUM_COST_PER_KM[rideType] + time * COST_PER_TIME[rideType]), MINIMUM_FARE[rideType]);
     }
-    public InvoiceSummary calculateFare(Ride[] rides, int userID){
+    public InvoiceSummary calculateFare(Ride[] rides, int userID, int rideType){
         double totalFare = 0;
         for(Ride ride : rides){
-           totalFare += this.calculateFare(ride.distance, ride.time);
+           totalFare += this.calculateFare(ride.distance, ride.time, rideType);
         }
         InvoiceSummary summary = new InvoiceSummary(rides.length, totalFare) ;
         RideRepository.addRide(userID, summary);
--- src/test/java/InvoiceServiceTest.java
+++ src/test/java/InvoiceServiceTest.java
@@ -17,12 +17,12 @@ public class InvoiceServiceTest {
 
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        Assertions.assertEquals(25.0, invoiceGenerator.calculateFare(2.0, 5));
+        Assertions.assertEquals(25.0, invoiceGenerator.calculateFare(2.0, 5, InvoiceService.NORMAL_RIDE));
     }
 
     @Test
     public void givenLessDistaneOrTime_ShouldReturnMinFare() {
-        Assertions.assertEquals(5, invoiceGenerator.calculateFare(0.1, 1));
+        Assertions.assertEquals(5, invoiceGenerator.calculateFare(0.1, 1,  InvoiceService.NORMAL_RIDE));
     }
 
     @Test
@@ -30,7 +30,7 @@ public class InvoiceServiceTest {
             Ride[] rides = {new Ride(2.0, 5),
                     new Ride(0.1, 1)
             };
-            InvoiceSummary summary = invoiceGenerator.calculateFare(rides, 10);
+            InvoiceSummary summary = invoiceGenerator.calculateFare(rides, 11,  InvoiceService.NORMAL_RIDE);
             InvoiceSummary expectedInvoiceSummary=  new InvoiceSummary(2,30.0);
             Assertions.assertEquals(expectedInvoiceSummary, summary);
     }
@@ -41,13 +41,22 @@ public class InvoiceServiceTest {
         Ride[] rides1 = {new Ride(2.0, 5),
                 new Ride(0.1, 1)
         };
-        InvoiceSummary summary = invoiceGenerator.calculateFare(rides1, 10);
+        InvoiceSummary summary = invoiceGenerator.calculateFare(rides1, 10,  InvoiceService.NORMAL_RIDE);
         expectedSummery.add(summary);
         Ride[] rides2 = {new Ride(2.0, 5),
                 new Ride(0.3, 1)
         };
-        summary = invoiceGenerator.calculateFare(rides2, 10);
+        summary = invoiceGenerator.calculateFare(rides2, 10,  InvoiceService.NORMAL_RIDE);
         expectedSummery.add(summary);
         Assertions.assertEquals(expectedSummery, RideRepository.getUserRideList(10));
+    }
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalFarePremiumCustomer() {
+        Ride[] rides = {new Ride(1.0, 5),
+                new Ride(2.0, 1)
+        };
+        InvoiceSummary summary = invoiceGenerator.calculateFare(rides, 10,  InvoiceService.PREMIUM_RIDE);
+        InvoiceSummary expectedInvoiceSummary=  new InvoiceSummary(2,57.0);
+        Assertions.assertEquals(expectedInvoiceSummary, summary);
     }
 }
